41037a21a73996882f519177255385bae13ccd3d
ilyailya22 <ilaMerihov@gmail.com>
2022-10-20 01:36:28 +0300

    create project

---
+++ ThreeInARow/Matrix.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace TreeInARow;
+
+public class Matrix
+{
+    private int _columns { get; set; }
+    private int _rows { get; set; }
+    private List<List<int>> _matrix { get; set; }
+
+    public void Show()
+    {
+        for (int j = _columns - 1; j >= 0; j--)
+        {
+            for (int i = 0; i < _matrix[i].Count; i++)
+            {
+                Console.Write(_matrix[i][j]); Console.Write(" ");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+
+    public void Vertical(ref int flag2)
+    {
+        Random rand = new Random();
+        int count = 1;
+        for (int j = 0; j < _columns; j++)
+        {
+            for (int i = 1; i < _matrix[j].Count; i++)
+            {
+                if (_matrix[j][i] == _matrix[j][i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count >= 3)
+                    {
+                        flag2++;
+                        var newMas = new List<int>();
+                        for (int k = 1; k <= count; k++)
+                        {
+                            _matrix[j].RemoveAt(i - k);
+                            newMas.Add(rand.Next(0, 4));
+                        }
+
+                        _matrix[j].AddRange(newMas);
+                    }
+
+                    count = 1;
+                }
+
+                if (i == _matrix[j].Count - 1)
+                {
+                    if (count >= 3)
+                    {
+                        flag2++;
+                        var newMas = new List<int>();
+
+                        for (int k = 1; k <= count; k++)
+                        {
+                            _matrix[j].RemoveAt(i - k + 1);
+                            newMas.Add(rand.Next(0, 4));
+                        }
+
+                        _matrix[j].AddRange(newMas);
+                    }
+
+                    count = 1;
+                }
+            }
+
+            count = 1;
+        }
+    }
+
+    public void Horisontal(ref int flag2)
+    {
+        int count = 1;
+        Random rand = new Random();
+        for (int j = 0; j < _rows; j++)
+        {
+            for (int i = 1; i < _matrix[j].Count; i++)
+            {
+                if (_matrix[i][j] == _matrix[i - 1][j])
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count >= 3)
+                    {
+                        flag2++;
+                        for (int k = 1; k <= count; k++)
+                        {
+                            _matrix[i - k].RemoveAt(j);
+                            _matrix[i - k].Add(rand.Next(0, 4));
+                        }
+
+                    }
+
+                    count = 1;
+                }
+
+                if (i == _matrix[j].Count - 1)
+                {
+                    if (count >= 3)
+                    {
+                        flag2++;
+                        for (int k = 1; k <= count; k++)
+                        {
+                            _matrix[i - k].RemoveAt(j);
+                            _matrix[i - k].Add(rand.Next(0, 4));
+                        }
+
+                    }
+
+                    count = 1;
+                }
+            }
+
+            count = 1;
+        }
+    }
+
+    public void Fill()
+    {
+        Random rand = new Random();
+        for (int j = 0; j < _columns; j++)
+        {
+            for (int i = 0; i < _rows; i++)
+            {
+                _matrix.Add(new List<int>());
+                _matrix[j].Add(rand.Next(0, 4));
+            }
+        }
+    }
+
+    public void Create(int columns, int rows)
+    {
+        _columns = columns;
+        _rows = rows;
+        _matrix = new List<List<int>>();
+    }
+
+}
---
+++ ThreeInARow/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace TreeInARow;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int flag1 = 1;
+        int flag2 = 2;
+        int colums = 9;
+        int rows = 9;
+        Matrix matrix = new Matrix();
+        matrix.Create(colums, rows);
+        matrix.Fill();
+        Console.WriteLine("Created matrix");
+        matrix.Show();
+        while (true)
+        {
+
+            Console.WriteLine("Vertical Shifts ");
+            matrix.Vertical(ref flag2);
+            matrix.Show();
+
+            Console.WriteLine("Horizontal Shifts ");
+            matrix.Horisontal(ref flag2);
+            matrix.Show();
+
+            if (flag1 == flag2) break;
+            flag1 = flag2;
+        }
+        Console.WriteLine("Result");
+        matrix.Show();
+
+    }
+}
